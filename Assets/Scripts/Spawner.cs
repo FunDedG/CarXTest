78823623a73031f2b0d8d6db205ba72d1eaeafb2
DenisOffical <denis16af@gmail.com>
2023-04-22 23:46:46 +0300

    feat: material Enemy

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,23 +3,24 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
-    public float m_interval = 3;
-    public GameObject m_moveTarget;
+	[SerializeField] private GameObject m_enemy;
+	public float interval = 3;
+    public GameObject moveTarget;
+	
 
     private float m_lastSpawn = -1;
 
     void Update()
     {
-        if (Time.time > m_lastSpawn + m_interval)
+        if (Time.time > m_lastSpawn + interval)
         {
-            var newMonster = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-            var r = newMonster.AddComponent<Rigidbody>();
-            r.useGravity = false;
-            newMonster.transform.position = transform.position;
-            var monsterBeh = newMonster.AddComponent<Monster>();
-            monsterBeh.m_moveTarget = m_moveTarget;
-
-            m_lastSpawn = Time.time;
+			//var newMonster = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+			//var r = newMonster.AddComponent<Rigidbody>();
+			//r.useGravity = false;
+			Instantiate(m_enemy, transform.position, Quaternion.identity);
+			var monsterBeh = m_enemy.GetComponent<Monster>();
+			monsterBeh.m_moveTarget = moveTarget;
+			m_lastSpawn = Time.time;
         }
     }
 }
