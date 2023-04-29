5cfd4ccc7cbc5e1c1a3c2a6b2acd53d56ba184ac
DenisOffical <denis16af@gmail.com>
2023-04-29 13:03:34 +0300

    fix: RotateSpeed

--- Assets/Scripts/Controller/CannonTowerController.cs
+++ Assets/Scripts/Controller/CannonTowerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace TestJob
 {
-	public class CannonTower : TowerBehaviour, IRotation
+	public class CannonTowerController : TowerBehaviour, IRotation
 	{
 		private RotationComponent m_rotationComponent;
 		public override void Start()
