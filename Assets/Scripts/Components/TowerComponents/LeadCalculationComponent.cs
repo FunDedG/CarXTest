146e63f51cc9a3f2f6c37bc506610791db47946a
DenisOffical <denis16af@gmail.com>
2023-05-08 16:52:53 +0300

    Revert "feat: first version ballistic tower"
    This reverts commit e5abf173b296e28264dcb5db753ba8db7fcd6b0f.

--- Assets/Scripts/Components/TowerComponents/LeadCalculationComponent.cs
+++ Assets/Scripts/Components/TowerComponents/LeadCalculationComponent.cs
@@ -8,55 +8,44 @@ namespace TestJob
     {
         public Vector3 PredictQuadratic(Vector3 shooterPosition, Vector3 targetPosition, Vector3 targetVelocity, float projectileSpeed)
         {
-			float time = PredictQuadraticTime(shooterPosition, targetPosition, targetVelocity, projectileSpeed);
-			Vector3 futureTargetPosition = targetPosition + targetVelocity * time;
-			Vector3 futureTargetDirection = futureTargetPosition - shooterPosition;
-			return futureTargetDirection;
-		}
+            Vector3 relativePosition = targetPosition - shooterPosition;
+            Vector3 relativeVelocity = targetVelocity;
 
-		public float PredictQuadraticTime(Vector3 shooterPosition, Vector3 targetPosition, Vector3 targetVelocity, float projectileSpeed)
-		{
-			Vector3 relativePosition = targetPosition - shooterPosition;
-			Vector3 relativeVelocity = targetVelocity;
+            float a = Vector3.Dot(relativeVelocity, relativeVelocity) - projectileSpeed * projectileSpeed;
+            float b = 2f * Vector3.Dot(relativePosition, relativeVelocity);
+            float c = Vector3.Dot(relativePosition, relativePosition);
 
-			float a = Vector3.Dot(relativeVelocity, relativeVelocity) - projectileSpeed * projectileSpeed;
-			float b = 2f * Vector3.Dot(relativePosition, relativeVelocity);
-			float c = Vector3.Dot(relativePosition, relativePosition);
-
-			float discriminant = b * b - 4f * a * c;
+            float discriminant = b * b - 4f * a * c;
 
 			if (discriminant < 0)
 			{
-				return 0;
+				return Vector3.zero;
 			}
 
 			float sqrtDiscriminant = Mathf.Sqrt(discriminant);
 
 			float xFirst = (-b + sqrtDiscriminant) / (2f * a);
-			float xSecond = (-b - sqrtDiscriminant) / (2f * a);
+            float xSecond = (-b - sqrtDiscriminant) / (2f * a);
 
-			if (xFirst < 0 && xSecond < 0)
-			{
-				return 0;
+            if (xFirst < 0 && xSecond < 0)
+            {
+                return Vector3.zero;
 			}
-			else if (xFirst < 0)
-			{
-				return xSecond;
-			}
-			else if (xSecond < 0)
-			{
-				return xFirst;
-			}
+            else if (xFirst < 0)
+            {
+                return targetPosition + targetVelocity * xSecond - shooterPosition;
+            }
+            else if (xSecond < 0)
+            {
+                return targetPosition + targetVelocity * xFirst - shooterPosition;
+            }
 			else
 			{
-				return Mathf.Min(xFirst, xSecond);
+				float projectileTimeToTarget = Mathf.Min(xFirst, xSecond);
+				Vector3 futureTargetPosition = targetPosition + targetVelocity * projectileTimeToTarget;
+				Vector3 futureTargetDirection = futureTargetPosition - shooterPosition;
+				return futureTargetDirection;
 			}
-			
 		}
-		public float CalculateAngle(float distance, float time, float gravity, float projectileSpeed)
-		{
-			float angle = Mathf.Atan((4 * gravity * distance) / (time * time * projectileSpeed * projectileSpeed));
-			return angle * Mathf.Rad2Deg;
-		}
-	}
+    }
 }
--- Assets/Scripts/Controller/CannonTowerController.cs
+++ Assets/Scripts/Controller/CannonTowerController.cs
@@ -8,10 +8,7 @@ namespace TestJob
     {
         private RotationComponent m_rotationComponent;
         private LeadCalculationComponent m_leadCalculationComponent;
-        public GameObject cannon;
-
-        // если переменная isBallistic false башня стреляет прямой наводкой, если true, то по баллистической траектории
-        public bool isBallistic = false;
+		public GameObject cannon;
 
         protected override void Start()
         {
@@ -20,16 +17,14 @@ namespace TestJob
             m_leadCalculationComponent = GetComponent<LeadCalculationComponent>();
             m_rotationComponent.Init(towerData, cannon);
         }
-
-        protected override void Update()
+		protected override void Update()
         {
-            base.Update();
-            RotateTower();
+			base.Update();
+			RotateTower();
         }
-
         public void RotateTower()
         {
-            if (searchEnemyComponent.GetTarget() && isBallistic == false)
+            if (searchEnemyComponent.GetTarget())
             {
                 Vector3 predictedPosition = m_leadCalculationComponent.PredictQuadratic(
                     projectilePosition.transform.position,
@@ -38,29 +33,6 @@ namespace TestJob
                     towerData.projectileSpeed
                 );
                 m_rotationComponent.Rotate(predictedPosition);
-            }
-
-            if (searchEnemyComponent.GetTarget() && isBallistic == true)
-            {
-                Vector3 predictedPositionBallistic = m_leadCalculationComponent.PredictQuadratic(
-                    projectilePosition.transform.position,
-                    searchEnemyComponent.GetTarget().transform.position,
-                    searchEnemyComponent.GetTarget().GetComponent<Rigidbody>().velocity,
-                    towerData.projectileSpeed
-                );
-                float time = m_leadCalculationComponent.PredictQuadraticTime(
-                    projectilePosition.transform.position,
-                    searchEnemyComponent.GetTarget().transform.position,
-                    searchEnemyComponent.GetTarget().GetComponent<Rigidbody>().velocity,
-                    towerData.projectileSpeed
-                );
-                float angle = m_leadCalculationComponent.CalculateAngle(
-                    predictedPositionBallistic.magnitude,
-                    time,
-                    9.8f,
-                    towerData.projectileSpeed
-                );
-                m_rotationComponent.RotateBallistic(predictedPositionBallistic, angle);
             }
         }
     }
